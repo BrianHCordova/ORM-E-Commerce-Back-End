7e4b6b39f13b005cbb271e99cc7ddfff87d9a3e3
briancordova@yahoo.com <briancordova@yahoo.com>
2024-04-24 13:00:44 -0700

    updated correct columns for Products

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,32 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+    }
   },
   {
     sequelize,
