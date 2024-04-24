0392617449aaa328f1f239fa694607c38708d066
briancordova@yahoo.com <briancordova@yahoo.com>
2024-04-24 12:28:17 -0700

    updated find all tags route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -122,7 +122,7 @@ router.delete('/:id', async (req, res) => {
       },
     });
     if (!productData) {
-      res.status(404).json({ message: 'No product found with that id!' });
+      res.status(404).json({ message: 'There is NO product found with that id!' });
       return;
     }
     res.status(200).json(productData);
