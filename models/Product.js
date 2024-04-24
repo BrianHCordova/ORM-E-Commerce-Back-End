8b4d00ad26add88d134df781fba4ecad1a431a5f
briancordova@yahoo.com <briancordova@yahoo.com>
2024-04-24 12:56:36 -0700

    updated error corrected for setting columns in wrong file for product tag

--- models/Product.js
+++ models/Product.js
@@ -10,26 +10,7 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'product',
-        key: 'id',
-      },
-    },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'tag',
-        key: 'id',
-      },
-    },
+    
   },
   {
     sequelize,
